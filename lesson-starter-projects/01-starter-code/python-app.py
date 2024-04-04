a32d9204ccd81f1a0baff2242c49f5f72be61e8b
rslim087a <42172016+rslim087a@users.noreply.github.com>
2024-04-04 11:00:29 -0400

    Update python-app.py

--- lesson-starter-projects/01-starter-code/python-app.py
+++ lesson-starter-projects/01-starter-code/python-app.py
@@ -6,3 +6,8 @@ print("   |_|    \\__, |\\__|_| |_|\\___/|_| |_(_)    ")
 print("          |___/                             ")
 print("                                            ")
 print("             -- Python --                   ")
+
+
+# Developer: Hey there, Captain DevOps! To run this application without Docker, simply execute:
+#            python python-app.py
+#            I trust that you can take it from here and work your container magic. Smooth sailing!
