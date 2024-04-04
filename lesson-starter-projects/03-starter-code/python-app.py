c9369cfdc424da2ce0ec2aee80f096d025c6c7ed
rslim087a <42172016+rslim087a@users.noreply.github.com>
2024-04-04 11:12:12 -0400

    Update python-app.py

--- lesson-starter-projects/03-starter-code/python-app.py
+++ lesson-starter-projects/03-starter-code/python-app.py
@@ -6,3 +6,7 @@ print("   |_|    \\__, |\\__|_| |_|\\___/|_| |_(_)    ")
 print("          |___/                             ")
 print("                                            ")
 print("             -- Python --                   ")
+
+# Developer: Captain DevOps, there's a Dockerfile here, but I'm not entirely sure how it works.
+#            I'm confident you can figure it out and make this application set sail smoothly.
+#            The helm is yours!
