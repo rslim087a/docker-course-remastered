2027c5212b46e4f0d5c7bf3f8c08a8d56512446f
rslim087a <42172016+rslim087a@users.noreply.github.com>
2024-04-10 16:52:45 -0400

    Update script.rb

--- workbook-starter-projects/02-starter-projects/ruby-user-input/script.rb
+++ workbook-starter-projects/02-starter-projects/ruby-user-input/script.rb
@@ -1,3 +1,22 @@
-print "Enter your name: "
-name = gets.chomp
-puts "Hello, #{name}!"
+quotes = [
+  "Believe you can and you're halfway there. - Theodore Roosevelt",
+  "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill",
+  "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+  "The only way to do great work is to love what you do. - Steve Jobs",
+  "Strive not to be a success, but rather to be of value. - Albert Einstein"
+]
+
+def print_quote(quote)
+  max_width = quote.lines.map(&:length).max + 4
+  border = "+" + "-" * (max_width - 2) + "+"
+
+  puts border
+  quote.lines.each do |line|
+    padding = " " * ((max_width - line.length - 2) / 2)
+    puts "|#{padding}#{line.strip.center(max_width - 4)}#{padding}|"
+  end
+  puts border
+end
+
+selected_quote = quotes.sample
+print_quote(selected_quote)
