5fb225a1acb40163969a7131eeed0252ff396a2b
rslim087a <42172016+rslim087a@users.noreply.github.com>
2024-05-02 16:47:03 -0400

    Update app.py

--- lesson-starter-projects/10-starter-code/grade-submission-portal/app.py
+++ lesson-starter-projects/10-starter-code/grade-submission-portal/app.py
@@ -7,6 +7,8 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 GRADE_SUBMISSION_API = os.environ.get('GRADE_SUBMISSION_API', 'localhost')
 GRADE_SERVICE_URL = f'{GRADE_SUBMISSION_API}'
 
+PORT = int(os.environ.get('PORT', 5001))
+
 @app.route('/', methods=['GET'])
 def get_form():
     return render_template('form.html')
@@ -34,4 +36,4 @@ def api_get_grades():
     return jsonify(grades)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=PORT)
